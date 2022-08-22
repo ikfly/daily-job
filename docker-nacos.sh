12755838678dabd2556302adcde3358fac84ebfe
zh-hq <zhang-hq34@qq.com>
2022-08-22 16:34:50 +0800

    nacos 启动内存降低

--- docker-nacos.sh
+++ docker-nacos.sh
@@ -5,8 +5,9 @@ docker  run \
 -p 9848:9848 \
 -p 9849:9849 \
 --restart=always \
--e JVM_XMS=256m \
--e JVM_XMX=256m \
+-e JVM_XMN=32m \
+-e JVM_XMS=32m \
+-e JVM_XMX=64m \
 -e MODE=standalone \
 -e PREFER_HOST_MODE=hostname \
 -v /opt/nacos/logs:/home/nacos/logs \
